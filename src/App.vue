03ca44413ec404036545339652e27a497a772e29
Шомуродов <cometa11we@gmail.com>
2025-10-09 03:15:58 +0500

    add: Footer.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import router from "./router";
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
       <Header />
       <RouterView />
+      <Footer />
 </template>
 
 <style scoped></style>
