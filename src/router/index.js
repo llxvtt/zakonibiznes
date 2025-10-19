3bc290830e6b5e1561bccf0bfa445df77720eaa2
Шомуродов <cometa11we@gmail.com>
2025-10-19 08:07:00 +0500

    add: scroll behaviour

--- src/router/index.js
+++ src/router/index.js
@@ -40,6 +40,13 @@ const router = createRouter({
                   component: () => import("../pages/ArticleDetail.vue"),
             },
       ],
+      scrollBehavior(to, from, savedPosition) {
+            if (savedPosition) {
+                  return savedPosition;
+            } else {
+                  return { top: 0 };
+            }
+      },
 });
 
 export default router;
